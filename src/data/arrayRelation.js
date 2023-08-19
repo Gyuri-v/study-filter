3c4a2d2a0a810d2ae7bbebb21f9dbfcf4cc8f71c
Gyuri-v <fnffkdll@naver.com>
2023-08-19 21:23:35 +0900

    3차 수정 완료

--- src/data/arrayRelation.js
+++ src/data/arrayRelation.js
@@ -6,22 +6,27 @@ const relation = [
       {
         label: '0% 미만',
         value: [-Infinity, 0],
+        checked: false,
       },
       {
         label: '0% 이상 ~ 10% 미만',
         value: [0, 10],
+        checked: true,
       },
       {
         label: '10% 이상 ~ 20% 미만',
         value: [10, 20],
+        checked: true,
       },
       {
         label: '20% 이상 ~ 30% 미만',
         value: [20, 30],
+        checked: true,
       },
       {
         label: '30% 이상',
         value: [30, Infinity],
+        checked: false,
       },
     ],
   },
@@ -32,48 +37,58 @@ const relation = [
       {
         label: '10 미만',
         value: [-Infinity, 10],
+        checked: false,
       },
       {
         label: '10 이상 ~ 50 미만',
         value: [10, 50],
+        checked: false,
       },
       {
         label: '50 이상 ~ 100 미만',
         value: [50, 100],
+        checked: true,
       },
       {
         label: '100 이상 ~ 200 미만',
         value: [100, 200],
+        checked: false,
       },
       {
         label: '200 이상',
         value: [200, Infinity],
+        checked: false,
       },
     ],
   },
   {
-    title: '처장조제액',
+    title: '처방조제액',
     name: 'sales',
     conditions: [
       {
         label: '10억 미만',
         value: [-Infinity, 1000000000],
+        checked: false,
       },
       {
         label: '10억 이상 ~ 50억 미만',
         value: [1000000000, 5000000000],
+        checked: true,
       },
       {
         label: '50억 이상 ~ 100억 미만',
         value: [5000000000, 10000000000],
+        checked: true,
       },
       {
         label: '100억 이상 ~ 300억 미만',
         value: [10000000000, 30000000000],
+        checked: false,
       },
       {
         label: '300억 이상',
         value: [30000000000, Infinity],
+        checked: false,
       },
     ],
   },
--- src/js/main.js
+++ src/js/main.js
@@ -1,168 +1,141 @@
-// import relation from '../data/arrayRelation' assert { type: "json" };
-// import datas from '../data/arrayData' assert { type: "json" };
+function createFilterList(data) {
+  const $list = document.createElement('li');
 
-function renderFilter(data, elem) {
-  data.forEach((item, index) => {
-    const $li = document.createElement('li');
+  const $title = document.createElement('strong');
+  $title.classList.add('title');
+  $title.innerText = data.title;
 
-    const $title = document.createElement('strong');
-    $title.classList.add('title');
-    $title.innerText = item.title;
+  const $conditions = document.createElement('div');
+  $conditions.classList.add('conditions');
+  $conditions.innerHTML = `
+    <input type="checkbox" name="${data.name}" id="${data.name}_all" value="-Infinity, Infinity">
+    <label for="${data.name}_all">전체</label>
+  `;
 
-    const $conditions = document.createElement('div');
-    $conditions.classList.add('conditions');
-    $conditions.innerHTML = `
-      <input type="checkbox" name="${item.name}_all" id="${item.name}_all">
-      <label for="${item.name}_all">전체</label>
-    `;
+  data.conditions.forEach((condition, index) => {
+    const $input = document.createElement('input');
+    $input.setAttribute('type', 'checkbox');
+    $input.setAttribute('name', data.name);
+    $input.setAttribute('id', `${data.name}_${index}`);
+    $input.setAttribute('value', condition.value);
+    $input.checked = condition.checked;
 
-    item.conditions.forEach((condition, index) => {
-      const $input = document.createElement('input');
-      $input.setAttribute('type', 'checkbox');
-      $input.setAttribute('name', item.name);
-      $input.setAttribute('id', `${item.name}_${index}`);
-      $input.setAttribute('value', condition.value);
+    const $label = document.createElement('label');
+    $label.setAttribute('for', `${data.name}_${index}`);
+    $label.innerText = condition.label;
 
-      const $label = document.createElement('label');
-      $label.setAttribute('for', `${item.name}_${index}`);
-      $label.innerText = condition.label;
-
-      $conditions.appendChild($input);
-      $conditions.appendChild($label);
-    });
-
-    $li.appendChild($title);
-    $li.appendChild($conditions);
-
-    elem.appendChild($li);
+    $conditions.appendChild($input);
+    $conditions.appendChild($label);
   });
+
+  $list.appendChild($title);
+  $list.appendChild($conditions);
+
+  return $list;
 }
 
-function renderList(data, elem) {
-  elem.innerHTML = '';
-  data.forEach((item, index) => {
-    const $tr = document.createElement('tr');
+function createTableList(data) {
+  const $elem = document.createElement('tr');
+  $elem.innerHTML = `
+      <td>${data.code}</td>
+      <td>${data.x}</td>
+      <td>${data.y}</td>
+      <td>${data.sales.toLocaleString()}</td>
+  `;
 
-    for (const key in item) {
-      if (Object.hasOwnProperty.call(item, key)) {
-        const values = item[key];
-        const $td = document.createElement('td');
-        $td.innerText = values;
-        $tr.appendChild($td);
-      }
-    }
-
-    elem.appendChild($tr);
-  });
+  return $elem;
 }
 
-function filterData(data, conditions) {
-  // const $inputsSeleted = $form.querySelectorAll('input:checked');
-  // const $inputsAll = $form.querySelectorAll('input[name*=all]');
-  let filteredData = [...data];
-  // let conditions = {};
-
-  // $inputsSeleted.forEach(item => {
-  //   const name = item.name;
-  //   const value = item.value.split(',');
-
-  //   if ( conditions[name] ) {
-  //     const array = [...value, ...conditions[name]];
-  //     const min = Math.min(...array);
-  //     const max = Math.max(...array);
-
-  //     conditions[name] = [min, max];
-  //   } else {
-  //     conditions[name] = [...value];
-  //   }
-  // });
-
-  // for (let i = 0; i < $inputsAll.length; i++) {
-  //   const $inputAll = $inputsAll[i];
-  //   const key = $inputAll.name.replace('_all', '');
-
-  //   if ( !conditions[key] ) {
-  //     $inputAll.click();
-  //   } else {
-  //     const value = conditions[key];
-
-  //     filteredData = filteredData.filter(item => {
-  //       if ( item[key] >= value[0] && item[key] < value[1] ) {
-  //         return true
-  //       }
-  //       return false;
-  //     });
-  //   }
-  // }
+function dataProcessing(data, conditions) {
+  const filteredData = data.filter(item => {
+    if (
+      item.x >= conditions.x[0] && item.x < conditions.x[1] &&
+      item.y >= conditions.y[0] && item.y < conditions.y[1] &&
+      item.sales >= conditions.sales[0] && item.sales < conditions.sales[1]
+    ) return true;
+  });
 
   return filteredData;
 }
 
+function conditionProcessing(data, conditions) {
+  data.map(item => {
+    const checkedCondition = item.conditions.filter(condition => condition.checked);
+    let conditionValues = [];
+
+    checkedCondition.forEach(condition => {
+      conditionValues.push(...condition.value)
+    });
+
+    conditions[item.name] = [
+      Math.min(...conditionValues),
+      Math.max(...conditionValues)
+    ];
+  });
+
+  return conditions;
+}
+
+// ---- main
 const main = function () {
   const $filterList = document.querySelector('.filter ul');
-  const $filterForm = document.querySelector('.filter-form');
   const $filterBtn = document.querySelector('.filter .btn-search');
-  const $listTable = document.querySelector('.list table tbody');
+  const $tableList = document.querySelector('.list table tbody');
 
   let conditions = { x: [], y: [], sales: [] };
+  let filteredData;
 
-  function onClickBtnFilter() {
-    const filteredData = filterData(datas);
-    renderList(filteredData, $listTable);
-  }
-
+  // * Functions
   function onClickInput(e) {
     const $target = e.currentTarget;
     const name = $target.name;
+    const idx = $target.id.split('_')[1];
 
-    const value = $target.value.split(',');
+    const relationObject = relation.find(item => item.name == name);
 
-    if ($target.checked) {
-      conditions[name] = [...conditions[name], ...value];
+    if ( idx == 'all' ) {
+      const $inputs = $filterList.querySelectorAll(`input[name="${name}"]:not([id*="all"])`);
+      $inputs.forEach(elem => elem.checked = false);
+
+      relationObject.conditions.map(item => item.checked = $target.checked);
     } else {
-      ////
+      relationObject.conditions[idx].checked = $target.checked;
     }
-
-    console.log(conditions);
-
-    // const $inputs = $filterList.querySelectorAll(`input[name="${name}"]:not([name*="all"])`);
-    // const $inputsSeleted = $filterList.querySelectorAll(`input[name="${name}"]:checked`);
-    // const $inputsAll = $filterList.querySelector(`input[name="${name}_all"]`);
-
-    // $inputsAll.checked = $inputsSeleted.length == $inputs.length ? true : false;
   }
 
-  function onClickInputAll(e) {
-    const $target = e.currentTarget;
-    const name = $target.name.replace('_all', '');
+  function onClickBtnFilter() {
+    conditions = conditionProcessing(relation, conditions);
+    filteredData = dataProcessing(datas, conditions);
 
-    const $inputs = $filterList.querySelectorAll(`input[name="${name}"]:not([name*="all"])`);
-    $inputs.forEach((item) => (item.checked = $target.checked));
+    $tableList.innerHTML = '';
+    filteredData.map(item => {
+      const $list = createTableList(item);
+      $tableList.appendChild($list);
+    });
   }
 
-  renderFilter(relation, $filterList);
+  // * Events
+  // condition 처리
+  conditions = conditionProcessing(relation, conditions);
 
-  const $inputs = $filterList.querySelectorAll('input:not([name*=all])');
-  const $inputsAll = $filterList.querySelectorAll('input[name*=all]');
-  $inputs.forEach((item) => item.addEventListener('click', onClickInput));
-  // $inputsAll.forEach((item) => item.addEventListener('click', onClickInputAll));
+  // data 처리
+  filteredData = dataProcessing(datas, conditions);
+
+  // list 생성
+  filteredData.map(item => {
+    const $list = createTableList(item);
+    $tableList.appendChild($list);
+  });
+  
+  // filter 생성
+  relation.map(item => {
+    const $list = createFilterList(item);
+    $filterList.appendChild($list);
+  });
+
+  // input 이벤트
+  const $inputs = $filterList.querySelectorAll('input');
+  $inputs.forEach(item => item.addEventListener('click', onClickInput));
   $filterBtn.addEventListener('click', onClickBtnFilter);
-  $filterBtn.click();
 };
 window.addEventListener('load', main);
-
-// * 리뷰
-// 초기 설정값 변경하고자 하면 -- 하면 전체가 클릭될 필요가 없지
-// 전체 일때는 나머지가 해제 되게끔
-// input의 체크될때마다 data를 따로
-// - 랑 + infinity 로 퉁
-// all 따로 갖고 있지 않음 -- input cheked 가 아닌 all이 아닌것들로 / 하나로 해결하게끔
-// 파라미터로 돔 말고, conditions 를 보내고 가져와서 돔에 뿌려주는거는 다르게
-// inputAll 을 안 쓰는 방향으로
-// renderFilte string 값만 return 되게 해서 innerHtml 을 안에서
-// dom 을 가져가지 않도록
-// elem 분리하고, 필터도 어떤 값이든 넣을 수 있게
-// 검색은 전체가 되게끔이 아니라 위의 상태값 그대로
-// _all 안쓰면 안생기게
-// hasOwnPropertys -> if ( values )
-// forEach ->
